703d7b710d9ab79bfbf00a8cd05a8c6c240500ca
sureshgadupu <gskforums@yahoo.com>
2020-07-07 18:43:07 +1200

    comments updated

--- src/main/java/com/test/latitude/Stock.java
+++ src/main/java/com/test/latitude/Stock.java
@@ -7,7 +7,7 @@ public class Stock
 	
 	public static int getMaxProfit(int[] stockPrices) {
 		
-		int maxProfit = 0; // consider initial maxprofit as 0 and we will return 0 when thre is no profit to be made 
+		int maxProfit = 0; // consider initial maxprofit as 0 and we will return 0 when there is no profit to be made 
 		int minSharePrice = stockPrices[0]; // lets consider first element of array as minimum share price
 		
 		for(int i = 1; i< stockPrices.length ; i++) {	
